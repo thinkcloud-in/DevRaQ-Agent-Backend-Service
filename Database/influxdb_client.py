ea6aedb32c0c8a29604cffff15bc2c3a05cac33b
KaibalyaNayak <knayak@thinkcloud.in>
2026-02-18 19:01:21 +0530

    influxdb name changed for deployment

--- Database/influxdb_client.py
+++ Database/influxdb_client.py
@@ -4,9 +4,9 @@ from utils.env_loader import load_env
 
 load_env()
 
-INFLUXDB_URL = os.getenv("INFLUXDB_URL")
-INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN")
-INFLUXDB_ORG = os.getenv("INFLUXDB_ORG")
+INFLUXDB_URL = os.getenv("INFLUXDB_URL", "https://influxdb.rcvdev.team")
+INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN", "9c00d5b8def8d01d15034e9ad5157f306b8dd8e9056a020cb74ba47538a23f2e")
+INFLUXDB_ORG = os.getenv("INFLUXDB_ORG", "RCV-Vamanit-ORG")
 
 def get_influxdb_client():
     return InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
